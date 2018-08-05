ca3354a03bff506ac5b7e5a07815ef0657fcd647
Your Name <you@example.com>
2018-08-05 11:50:43 +0200

    adding comments and removing unrelated links

--- src/AppBundle/Service/Edit/Append/ImageMd.php
+++ src/AppBundle/Service/Edit/Append/ImageMd.php
@@ -7,6 +7,7 @@ use AppBundle\Entity\Cars;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
+//class used for photo upload
 class ImageMd {
 
     protected $entityManager;
@@ -15,11 +16,8 @@ class ImageMd {
     protected $container;
     public $errmove = array();
 
-    public function __construct(EntityManager $em, RequestStack $requestStack, ContainerInterface $container) {
-        $this->entityManager = $em;
+    public function __construct(RequestStack $requestStack) {
         $this->requestStack = $requestStack;
-        $this->container = $container;
-        $this->user_active = $container->get('security.token_storage')->getToken()->getUser();
     }
 
     public function CreateImgMsg($id) {
@@ -28,6 +26,7 @@ class ImageMd {
         return $this;
     }
 
+    //moves the main picture of the avatar according to id number of the advert added
     public function CreateAwatar($id) {
         $val_errors = null;
         try {
@@ -38,6 +37,8 @@ class ImageMd {
         return $this;
     }
 
+    //creates a folder for the selected photo id and takes photos there that were 
+    //sort files after uploady photos
     public function CreateSortFilesImg($id) {
         if (isset($this->requestStack->getCurrentRequest()->files->get('form')['image'])) {
             if (!is_dir("../web/images/" . $id)) {
@@ -73,6 +74,7 @@ class ImageMd {
         return $this;
     }
 
+    // check if there were any errors during the action
     public function isErrors() {
         if (in_array(true, $this->errmove)) {
             return true;
@@ -80,6 +82,7 @@ class ImageMd {
         return false;
     }
 
+    // returns an array with errors
     public function getErrors() {
         return $this->errmove;
     }
