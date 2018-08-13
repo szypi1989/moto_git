35b506619c4ecd39c11eb214fe599884b9a90fdb
Your Name <you@example.com>
2018-08-13 20:18:39 +0200

    repair por2

--- src/AppBundle/Service/Defaults/Por.php
+++ src/AppBundle/Service/Defaults/Por.php
@@ -11,4 +11,4 @@ $paginator = $this->get('knp_paginator');
 $pagination = $paginator->paginate(
         $query, /* query NOT result */ $request->query->getInt('page', 1)/* page number */, 5/* limit per page */, array('defaultSortFieldName' => 'a.model', 'defaultSortDirection' => 'asc')
 );
-return array('pagination' => $pagination, 'filtr' => $array_par, "transfiltr" => $trans_arr, "transsort" => $trans_sort); d
+return array('pagination' => $pagination, 'filtr' => $array_par, "transfiltr" => $trans_arr, "transsort" => $trans_sort);
