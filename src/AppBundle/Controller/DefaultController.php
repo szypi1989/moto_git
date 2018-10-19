5a9cffa2efe5144a3ddc1be8320ee62b285a0c09
Your Name <you@example.com>
2018-10-19 12:05:58 +0200

    repair data

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -9,21 +9,24 @@ use Symfony\Component\HttpFoundation\Request;
 use AppBundle\Entity\Cars;
 use Doctrine\ORM\EntityManager;
 use AppBundle\Service\Defaults\Index\PutAdvert;
-
+use AppBundle\Service\Defaults\Index\CarCriteriaRequest;
+use AppBundle\Service\RequestControl as RequestControl;
+use AppBundle\Service\Defaults\Index\FetchPaginer;
+use AppBundle\Service\Defaults\Index\FetchMsgSql;
 class DefaultController extends Controller {
 
     /**
      * @Route("/", name="index")
      * @Template()
      */
-    public function indexAction(Request $request, PutAdvert $putadvert, EntityManager $em) {
+    public function indexAction(Request $request, PutAdvert $putadvert,EntityManager $em,CarCriteriaRequest $criteria,RequestControl $requestcntrl,FetchPaginer $paginer,FetchMsgSql $fetchmsgsql) {
         if ($request->getMethod() == 'POST') {
             // put search module
-            return array('pagination' => $putadvert->getPagination(), 'filtr' => $putadvert->paginer->FetchMsgSql->build_condition(), "transfiltr" => $putadvert->getTransArr(), "transsort" => $putadvert->getTransSort());
+            return array('pagination' => $putadvert->getPagination(), 'filtr' => $putadvert->paginer->FetchMsgSql->criteriarequest->putDataPost(), "transfiltr" => $putadvert->getTransArr(), "transsort" => $putadvert->getTransSort());
         } else {
             // append data post 
             if ($request->query->get('search') == "1") {
-                return array('pagination' => $putadvert->getPagination(), 'filtr' => $putadvert->paginer->FetchMsgSql->build_condition(), "transfiltr" => $putadvert->getTransArr(), "transsort" => $putadvert->getTransSort());
+                return array('pagination' => $putadvert->getPagination(), 'filtr' => $putadvert->paginer->FetchMsgSql->criteriarequest->pushDataGet(), "transfiltr" => $putadvert->getTransArr(), "transsort" => $putadvert->getTransSort());
             } else {
                 // put all data cars
                 return array('pagination' => $putadvert->getPagination(), "transsort" => $putadvert->getTransSort());
